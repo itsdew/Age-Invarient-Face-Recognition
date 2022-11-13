8f91641874eec1eabab36503ff6df69c74e83911
Devendra Uraon <devendra123world@gmail.com>
2022-11-13 13:08:29 +0530

    Add files via upload

---
+++ Age-Invarient Face Recognition/Source_code.m
@@ -0,0 +1,7 @@
+% 
+% AGE INVARIANT FACE RECOGNITION SYSTEM
+%
+% email  devendra123world@gmail.com
+% mobile +91 6266108841
+% link http://www.advancedsourcecode.com
+%
---
+++ Age-Invarient Face Recognition/readme.m
@@ -0,0 +1,48 @@
+% 
+% FUNCTIONS % How to use:
+%
+% Select image:
+%              read the input image.
+%
+% Add selected image to database with manual landmark points:   
+%              the input image is added to database and will be used for
+%              training. Class number (a progressive integer number) 
+%              must be specified. Landmark points have to be
+%              manually selected on image (68 points). See FG-NET AGING DATABASE for
+%              landmark points positionings. See also example images with
+%              landmark points.
+%
+% Add selected image to database with point file selection:
+%              the input image is added to database and will be used for
+%              training. Class number (a progressive integer number) 
+%              must be specified. A file with landmark points has to be
+%              selected with dialog box. See FG-NET AGING DATABASE for
+%              landmark points positionings and file format.
+%
+% Database Info:                                  
+%              show informations about the images present in database.
+%
+% Face Recognition with manual landmark points:                        
+%              face recognition. The selected input image is processed.
+%              Landmark points have to be manually selected on image (68
+%              points). See FG-NET AGING DATABASE for
+%              landmark points positionings. See also example images with
+%              landmark points. Code returns face ID.
+%
+% Face Recognition with point file selection:
+%              face recognition. The selected input image is processed. A file with landmark 
+%              points has to be selected with dialog box. See FG-NET AGING DATABASE for
+%              landmark points positionings and file format. Code returns
+%              face ID.
+%
+% Delete Database:
+%              remove database from the current directory
+%
+% Source code for Age Invariant Face Recognition: 
+%              Visit my github profile Github.com/itsdew
+%
+% Exit:
+%              quit program
+%
+% 
+% 
