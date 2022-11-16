88f51699ccf57879861fb2f61dadb9d58891e2c7
Devendra Uraon <devendra123world@gmail.com>
2022-11-16 22:30:44 +0530

    Add files via upload

---
+++ Age-Invarient Face Recognition/sourcecode.m
@@ -0,0 +1,18 @@
+% 
+% AGE INVARIANT FACE RECOGNITION SYSTEM
+% 
+% In order to obtain the complete source code for Age Invariant Face Recognition System
+% please visit my website
+% 
+% http://www.advancedsourcecode.com/facerecage.asp
+% 
+% For any question please email me luigi.rosa@tiscali.it
+%
+% Luigi Rosa
+% Via Centrale 35
+% 67042 Civita di Bagno
+% L'Aquila - ITALY 
+% email  luigi.rosa@tiscali.it
+% mobile +39 3207214179
+% website http://www.advancedsourcecode.com
+%
