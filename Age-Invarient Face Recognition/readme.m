3dc56a74231cc479d274003ca8b28cb53b1dd810
Devendra Uraon <devendra123world@gmail.com>
2023-12-03 01:19:19 +0530

    Update readme.m

--- Age-Invarient Face Recognition/readme.m
+++ Age-Invarient Face Recognition/readme.m
@@ -1,1 +1,9 @@
-
+% In order to launch the code just do the following steps:
+% 
+% - run Matlab
+% - unzip all files in Matlab current directory (C:/..../Matlab/work is the default directory)
+% - type the name of main function ("faceage") on Matlab command window (the white window)
+% - now a simple and intuitive GUI should appear
+%
+% Code has been tested on FG-NET AGING DATABASE
+% http://www.fgnet.rsunit.com/
