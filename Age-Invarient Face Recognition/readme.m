564ccdb6a507ef311096e768c3b46a1d70bba3fe
Devendra Uraon <devendra123world@gmail.com>
2023-12-03 01:19:53 +0530

    Update readme.m

--- Age-Invarient Face Recognition/readme.m
+++ Age-Invarient Face Recognition/readme.m
@@ -7,3 +7,16 @@
 %
 % Code has been tested on FG-NET AGING DATABASE
 % http://www.fgnet.rsunit.com/
+%
+% FUNCTIONS
+%
+% Select image:
+%              read the input image.
+%
+% Add selected image to database with manual landmark points:   
+%              the input image is added to database and will be used for
+%              training. Class number (a progressive integer number) 
+%              must be specified. Landmark points have to be
+%              manually selected on image (68 points). See FG-NET AGING DATABASE for
+%              landmark points positionings. See also example images with
+%              landmark points.
